5503b01d8d26b859e741bb7ac44c44805572f2e8
Dauren <dake.buy@gmail.com>
2022-12-02 23:03:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let numberOfFilms = 100;
+
+console.log(numberOfFilms);
+
+
+const personalMovieDB = {
+	count: 100,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const q = prompt('Один из последних фильом', '');
+
+
+
