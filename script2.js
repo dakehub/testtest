e660f24b08078bbd9e879b252c96d5d11d8b8240
Dauren <dake.buy@gmail.com>
2022-12-04 12:48:18 +0500

    create personalMovieDB

--- script2.js
+++ script2.js
@@ -11,14 +11,11 @@ const personalMovieDB = {
 };
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('На сколько оцените его?', '');
+		b = prompt('На сколько оцените его', ''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
-
-
-
